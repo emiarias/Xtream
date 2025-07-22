7f4573115c720a0afed421cb4177145e3c724061
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-21 23:39:07 -0300

    agregue validaciones al login de email y contraseña

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Col, Form, Row } from "react-bootstrap";
+import { Col, Form, Row, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
@@ -19,14 +19,38 @@ const Login = () => {
               <Form.Control
                 type="email"
                 placeholder="Ej: juan2025@mail.com"
+                {...register("email", {
+                  required: "El correo electrónico es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El correo electrónico debe tener un formato válido, por ejemplo juan2025@mail.com",
+                  },
+                })}
               ></Form.Control>
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mt-3">
               <Form.Label>Contraseña</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Ingresa tu contraseña"
+                {...register("password", {
+                  required: "La contraseña es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                    message:
+                      "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.",
+                  },
+                })}
               ></Form.Control>
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mt-3">
               <Form.Check
@@ -35,6 +59,9 @@ const Login = () => {
                 label="Recordar mi usuario y contraseña"
               ></Form.Check>
             </Form.Group>
+            <Button variant="success" type="submit" className="mt-4">
+              Iniciar sesión
+            </Button>
           </Form>
         </Col>
         <Col></Col>
