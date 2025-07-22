2f492639d54ee55130a0e64a7ad86c313b2a1969
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-21 23:34:17 -0300

    agregue y maquete el form

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,15 +1,46 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <section className="container">
-            <h1 className="my-3 text-center tinos">Inicia sesión</h1>
-            <Row xs={1} md={2}>
-                <Col></Col>
-                <Col></Col>
-            </Row>
-        </section>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <section className="container">
+      <h1 className="my-3 text-center tinos">Inicia sesión</h1>
+      <Row xs={1} md={2}>
+        <Col>
+          <Form className="raleway">
+            <Form.Group className="mt-3">
+              <Form.Label>Correo electrónico</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ej: juan2025@mail.com"
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className="mt-3">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ingresa tu contraseña"
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group className="mt-3">
+              <Form.Check
+                type="switch"
+                id="custom-switch"
+                label="Recordar mi usuario y contraseña"
+              ></Form.Check>
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+    </section>
+  );
 };
 
 export default Login;
