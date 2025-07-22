91b59d5c9f49364e3002aee91bc7f90ff24269cf
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-21 23:20:51 -0300

    cree componente de login y agregue titulo

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,15 @@
+import { Col, Row } from "react-bootstrap";
+
+const Login = () => {
+    return (
+        <section className="container">
+            <h1 className="my-3 text-center tinos">Inicia sesión</h1>
+            <Row xs={1} md={2}>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+        </section>
+    );
+};
+
+export default Login;
