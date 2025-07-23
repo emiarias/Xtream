4cbce34f7647ec589d25d437ec8abf9d317235f8
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-22 23:26:00 -0300

    cree componentes de administrador, detalle pelicula y acerca de nosotros

---
+++ src/components/pages/DetallePelicula.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetallePelicula = () => {
+    return (
+        <div>
+            <h1>Detalle</h1>
+        </div>
+    );
+};
+
+export default DetallePelicula;
