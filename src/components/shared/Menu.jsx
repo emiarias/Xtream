66cb4b0a4323857253e0f5655c927ef3c0f452ec
Lucasfi <lukafigueroa64@gmail.com>
2025-07-23 16:39:03 -0300

    agregue formulario al modal creaTuCuenta y agregue react-hook-form

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -4,13 +4,19 @@ import Navbar from "react-bootstrap/Navbar";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Button } from "react-bootstrap";
+import { Button, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 import { NavLink } from "react-router";
-import { useState } from 'react';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import Modal from 'react-bootstrap/Modal';
+import Modal from "react-bootstrap/Modal";
 
 const Menu = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -50,7 +56,10 @@ const Menu = () => {
                 <NavLink to={"/administrador"} className="tinos nav-link">
                   Administrador
                 </NavLink>
-                <Button className="tinos nav-link boton-crea-cuenta" onClick={handleShow}>
+                <Button
+                  className="tinos nav-link boton-crea-cuenta"
+                  onClick={handleShow}
+                >
                   Crea tu cuenta
                 </Button>
                 <Button className="tinos nav-link boton-logout w-25">
@@ -63,17 +72,63 @@ const Menu = () => {
       </header>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>
+            ¿Quieres ver Xtream ya? completa los campos para crear una cuenta
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit((e) => e.preventDefault)}>
+            <FormGroup className="mb-3" controlId="formBasicNombreUsuario">
+              <FormLabel>
+                Nombre Usuario
+              </FormLabel>
+              <FormControl
+                type="text"
+                placeholder="ej: user1">
+              </FormControl>
+            </FormGroup>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ej: Xtream@gmail.com"
+                maxLength={100}
+                min={5}
+                {...register("email", {
+                  required: "El correo electrónico es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El correo electrónico debe tener un formato válido, por ejemplo juan2025@mail.com",
+                  },
+                  minLength: {
+                    value: 5,
+                    message:
+                      "La descrición amplia debe tener almenos 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 100,
+                    message:
+                      "La descrición amplia debe tener como máximo 100 caracteres",
+                  },
+                })}
+              ></Form.Control>
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Ingresa tu contraseña" />
+            </Form.Group>
+            <FormGroup className="d-flex justify-content-end">
+              <Button variant="success" type="submit">
+                Save Changes
+              </Button>
+            </FormGroup>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
