d4679432657b4ca44e412bd64ef3567f75180cbf
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-24 17:41:23 -0300

    maquetado del form de agregar/editar pelicula

--- src/Components/pages/FormularioPelicula.jsx
+++ src/Components/pages/FormularioPelicula.jsx
@@ -1,4 +1,4 @@
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const FormularioPelicula = ({ titulo }) => {
@@ -13,7 +13,7 @@ const FormularioPelicula = ({ titulo }) => {
   return (
     <section className="container my-5">
       <h1 className="my-3">{titulo}</h1>
-      <Form>
+      <Form onSubmit={handleSubmit((e) => e.preventDefault)}>
         <Form.Group className="mb-3">
           <Form.Label>Nombre*</Form.Label>
           <Form.Control
@@ -62,23 +62,15 @@ const FormularioPelicula = ({ titulo }) => {
             type="number"
             placeholder="Ej: 1999"
             {...register("anioEstreno", {
-              required: "El director es un dato obligatorio",
-              minLength: {
-                value: 1,
-                message: "El director debe tener al menos 1 caracteres",
-              },
-              maxLength: {
-                value: 4,
-                message: "El director debe tener como maximo 4 caracteres",
-              },
+              required: "El año de estreno es un dato obligatorio",
               min: {
-              value: 1800,
-              message: "El año mínimo es el 1800",
+                value: 1800,
+                message: "El año de estreno debe estar entre 1800 y 2025",
               },
               max: {
-              value: 2025,
-              message: "El año máximo es el 2025",
-              }
+                value: 2025,
+                message: "El año de estreno debe estar entre 1800 y 2025",
+              },
             })}
           />
           <Form.Text className="text-danger">
@@ -89,12 +81,11 @@ const FormularioPelicula = ({ titulo }) => {
           <Form.Label>Poster*</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Ej: https://www.movieposters.com/cdn/shop/products/ed4796ac6feff9d2a6115406f964c928_6b200bda-fe71-4900-ad7f-903cdda50dab_480x.progressive.jpg"
+            placeholder="Ej: https://www.movieposters.com/cdn/shop/products/ed4796ac6feff9d2a6115406f964c928_6b200bda-fe71-4900-ad7f-903cdda50dab_480x.jpg"
             {...register("imagen", {
               required: "La url de la imagen es un dato obligatorio",
               pattern: {
-                value:
-                  /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?(\.(jpg|jpeg|png|webp))$/,
+                value: /^https?:\/\/.+\.(jpg|jpeg|png|gif)(\?.*)?$/,
                 message:
                   "La imagen debe ser una url de imagen valida terminada en (jpg|jpeg|png|webp)",
               },
@@ -145,7 +136,7 @@ const FormularioPelicula = ({ titulo }) => {
             type="text"
             placeholder="Ej: Película de acción y fantasía sobre la realidad simulada."
             as="textarea"
-            {...register("formDescripcionBreve", {
+            {...register("descripcionBreve", {
               required: "La descripcion breve es un dato obligatorio",
               minLength: {
                 value: 2,
@@ -160,7 +151,7 @@ const FormularioPelicula = ({ titulo }) => {
             })}
           />
           <Form.Text className="text-danger">
-            {errors.formDescripcionBreve?.message}
+            {errors.descripcionBreve?.message}
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3">
@@ -169,7 +160,7 @@ const FormularioPelicula = ({ titulo }) => {
             type="text"
             placeholder="Ej: Un hacker descubre que su realidad es una simulación creada por máquinas inteligentes y se une a la rebelión para liberar a la humanidad."
             as="textarea"
-            {...register("formDescripcionAmplia", {
+            {...register("descripcionAmplia", {
               required: "La descripcion amplia es un dato obligatorio",
               minLength: {
                 value: 2,
@@ -184,8 +175,13 @@ const FormularioPelicula = ({ titulo }) => {
             })}
           />
           <Form.Text className="text-danger">
-            {errors.formDescripcionAmplia?.message}
+            {errors.descripcionAmplia?.message}
           </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-4">
+          <Button variant="success" type="submit">
+            Guardar
+          </Button>
         </Form.Group>
       </Form>
     </section>
